3e945143d67d3b02c64c3bf40cc34e5cdea4da29
Andréas Anemyr <aa223ig@student.lnu.se>
2014-09-09 15:57:06 +0200

    Nu är 9:an på gång, Får jobba vidare sedan :)

---
+++ 1-9-en-array-baklanges/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_9_en_array_baklanges
+{
+    class Program
+    {
+
+        static void testMetod() {
+            Console.WriteLine("Det");
+        }
+
+        static void Main(string[] args)
+        {
+            byte totalNumbers;
+
+            //User fills in a number for numbers
+            testMetod();
+
+
+            //Mata in talen till en Array
+
+            //Arrayen ska därefter skrivas ut baklänges
+
+            
+
+
+
+        }
+    }
+}
