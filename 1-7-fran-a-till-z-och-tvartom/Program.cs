94bfa958a2437479e905f6fd2ea8a4f8499323ce
Andreas Anemyr <aa223ig@NY108X01.lnu.se>
2014-09-09 15:18:45 +0200

    x

--- 1-7-fran-a-till-z-och-tvartom/Program.cs
+++ 1-7-fran-a-till-z-och-tvartom/Program.cs
@@ -10,6 +10,28 @@ namespace _1_7_fran_a_till_z_och_tvartom
     {
         static void Main(string[] args)
         {
+
+
+
+            for (byte letter = 65; letter <= 90; letter++)
+            {
+               Console.Write((char)(letter+32));
+               Console.Write((char)(letter));
+            }
+
+            Console.WriteLine();
+
+            for (byte letterBackward = 90; letterBackward >= 65; letterBackward--)
+            {
+                Console.Write((char)(letterBackward+32));
+                Console.Write((char)(letterBackward));
+            }
+
+            Console.WriteLine();
+            
+
+
+
         }
     }
 }
