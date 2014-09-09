723b0d14bedc8fbbed74bc9ec54ddf121b372eca
Andreas Anemyr <aa223ig@NY108X01.lnu.se>
2014-09-09 14:36:48 +0200

    Nu är övning nummer 6 färdig. Strulade med Int som jag bytte till long....

---
+++ 1-6-produkten-av-heltal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_6_produkten_av_heltal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            long product = 1;
+       
+
+            for (int i = 1; i <= 20 ; i++)
+            {
+
+                product *= i;
+            }
+        Console.WriteLine("Summan av alla heltal mellan 1 och 20 är {0}", product);
+        }
+    }
+}
