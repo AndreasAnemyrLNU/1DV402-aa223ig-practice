2f82398bb6cdea0c32bf0176849cd8c327c24a67
Andréas Anemyr <aa223ig@student.lnu.se>
2014-09-09 15:42:01 +0200

    Nu går det någorlunda bra. Kärde en foreach på övning g nr åtta. Att komma ihåg i fortsättningen är att enge datatyp för det temporära värdet i en foreach...

---
+++ 1-8-dubbelt-upp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_8_dubbelt_upp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valueHolder = {10,23,5,1,15};
+
+            foreach (int value in valueHolder){
+                Console.Write("{0} ",value);            
+            }
+
+            valueHolder[1] *= 2;
+            valueHolder[4] *= 2;
+
+            Console.WriteLine();
+
+            foreach (int value in valueHolder)
+            {
+                Console.Write("{0} ", value);
+            }
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
